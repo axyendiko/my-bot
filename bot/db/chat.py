701de32be1177183672a22aac3dff1d86efd4396
diaszhaksygeldiev <zhaksygeldiev@gmail.com>
2025-10-10 00:37:39 +0500

    as

--- bot/db/chat.py
+++ bot/db/chat.py
@@ -1,9 +1,9 @@
 from sqlalchemy import Column, Integer, BigInteger, DateTime, Sequence
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.future import select  # Для асинхронных запросов
+from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 from sqlalchemy.exc import NoResultFound
-
 
 Base = declarative_base()
 
@@ -15,17 +15,26 @@ class Chat(Base):
     creation_date = Column(DateTime, default=datetime.utcnow, nullable=True)
     upd_date = Column(DateTime, default=None, onupdate=datetime.utcnow, nullable=True)
 
+# Исправленная асинхронная функция getChatId
 async def getChatId(chatId: int, session_maker: sessionmaker):
     async with session_maker() as session:
         try:
-        # Пытаемся найти запись с указанным chat_id
-            chat = session.query(Chat).filter_by(chat_id=chat_id).one()
-            return chat.id
+            # Пытаемся найти запись с указанным chat_id
+            result = await session.execute(select(Chat).filter_by(chat_id=chatId))
+            chat = result.scalars().first()  # Извлекаем первый результат
+            if chat:
+                return chat.id
+            else:
+                # Если запись не найдена, создаём новую
+                new_chat = Chat(chat_id=chatId, creation_date=datetime.utcnow())
+                session.add(new_chat)
+                await session.commit()  # Асинхронно сохраняем изменения
+                await session.refresh(new_chat)  # Обновляем объект, чтобы получить его id
+                return new_chat.id
         except NoResultFound:
             # Если запись не найдена, создаём новую
-            new_chat = Chat(chat_id=chat_id, creation_date=datetime.utcnow())
+            new_chat = Chat(chat_id=chatId, creation_date=datetime.utcnow())
             session.add(new_chat)
-            session.commit()  # Сохраняем изменения в базе данных
-            session.refresh(new_chat)  # Обновляем объект, чтобы получить его id
+            await session.commit()  # Асинхронно сохраняем изменения
+            await session.refresh(new_chat)  # Обновляем объект, чтобы получить его id
             return new_chat.id
-
