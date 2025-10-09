a5b0dda8da38a2a5ceac89cd2d1cccf113bcd801
diaszhaksygeldiev <zhaksygeldiev@gmail.com>
2025-10-09 20:10:25 +0500

    ass

--- bot/db/chat.py
+++ bot/db/chat.py
@@ -16,13 +16,13 @@ async def getChatId(chatId: int, session_maker: sessionmaker):
     async with session_maker() as session:
         try:
         # Пытаемся найти запись с указанным chat_id
-        chat = session.query(Chat).filter_by(chat_id=chat_id).one()
-        return chat.id
-    except NoResultFound:
-        # Если запись не найдена, создаём новую
-        new_chat = Chat(chat_id=chat_id, creation_date=datetime.utcnow())
-        session.add(new_chat)
-        session.commit()  # Сохраняем изменения в базе данных
-        session.refresh(new_chat)  # Обновляем объект, чтобы получить его id
-        return new_chat.id
+            chat = session.query(Chat).filter_by(chat_id=chat_id).one()
+            return chat.id
+        except NoResultFound:
+            # Если запись не найдена, создаём новую
+            new_chat = Chat(chat_id=chat_id, creation_date=datetime.utcnow())
+            session.add(new_chat)
+            session.commit()  # Сохраняем изменения в базе данных
+            session.refresh(new_chat)  # Обновляем объект, чтобы получить его id
+            return new_chat.id
 
